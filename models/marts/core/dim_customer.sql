8c889140b872b48318c3782840ea7414ac8e5e16
Deekshith lobo <deekshithlobo29@gmail.com>
2023-01-18 11:58:32 +0000

    hmm

--- models/marts/core/dim_customer.sql
+++ models/marts/core/dim_customer.sql
@@ -1,9 +1,3 @@
-{{
-    config(
-        materialized='table'
-    )
-}}
-
 with customers as (
 
     select * from {{
@@ -45,7 +39,7 @@ final as (
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
+    
     from customers
 
     left join customer_orders using (customer_id)
